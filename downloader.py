94360eded6ec641f86d5c16e670160a2380d2688
Jiří Vlasák <jirivlasak9@gmail.com>
2021-02-24 23:00:07 +0100

    Added file download example

--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.client_context import ClientContext
@@ -25,7 +26,7 @@ def listFolder(path):
         print(file.properties["ServerRelativeUrl"])
 
 
-site_url = "https://vutbr.sharepoint.com/sites/IZU/"
+site_url = "https://vutbr.sharepoint.com/sites/IPK2020L/"
 
 
 ctx = ClientContext(site_url).with_credentials(
@@ -34,4 +35,12 @@ web = ctx.web
 ctx.load(web)
 ctx.execute_query()
 
-listFolder('Sdilene dokumenty/')
+listFolder('Sdilene dokumenty/General/Recordings')
+
+file_url = "/sites/IPK2020L/Sdilene dokumenty/General/Recordings/Meeting in _General_-20210209_090016-Meeting Recording.mp4"
+download_path = "test.mp4"
+with open(download_path, "wb") as local_file:
+    file = ctx.web.get_file_by_server_relative_url(
+        file_url).download(local_file).execute_query()
+
+print("[Ok] file has been downloaded: {0}".format(download_path))
